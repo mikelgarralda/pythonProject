773906303262a731dfe3cdfb106e1795447a7371
Mikel Garralda Ruiz <mgarralda96@gmail.com>
2020-10-19 21:04:14 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+np.set_printoptions(precision=4)
+
+
+if __name__ == '__main__':
+    print(f'Hola!')
